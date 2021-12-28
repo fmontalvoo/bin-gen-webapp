1a632692e06817d9d590d370e8de34a5bc297c16
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-12-28 17:37:17 -0500

    Se actualiza Angular.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/pages/generador/services/card-generator/card-generator.service.ts
+++ src/app/pages/generador/services/card-generator/card-generator.service.ts
@@ -8,71 +8,56 @@ export class CardGeneratorService {
 
   constructor() { }
 
-  public random(min: number, max: number) {
+  public random(min: number, max: number): number {
     return Math.floor((Math.random() * (max - min + 1)) + min);
   }
 
-  public generateCVV() {
+  public generateCVV(): string {
     return `${this.random(0, 9)}${this.random(0, 9)}${this.random(0, 9)}`;
   }
 
-  public generateValidThru() {
+  public generateValidThru(): string {
     let month = this.random(1, 12);
     const year = new Date().getFullYear();
     let yearRange = this.random(1, 4);
     return `${month < 10 ? '0' + month : month}|${year + yearRange}`
   }
 
-  // public generator({ number, valid, cvv, range, randomG, digits }) {
-  //   let limit = (digits == 'true') ? 15 : 16;
-  //   let cards = [];
-  //   let min = limit > 15 ? 6 : 5;
-  //   if (randomG == 'true' && number.length <= limit - 1) {
-  //     if (number.length >= min) {
-  //       for (let i = 0; i < range; i++)
-  //         cards.push(this.generateRandomCard({ number: number, valid: valid, cvv: cvv, limit: (limit - 1) }));
-  //     }
-  //   }
-  //   if (randomG == 'false' && number.length <= limit - 1) {
-  //     cards = this.generateSequencialCard({ number: number, valid: valid, cvv: cvv, range: range, limit: (limit - 1) });
-  //   }
-  //   return cards;
-  // }
 
-  // public generateRandomCard({ number, valid, cvv, limit }) {
-  //   const total = limit - number.length;
-  //   let digits = ''
-  //   for (let i = 0; i < total; i++)
-  //     digits += this.random(0, 9);
+  public generateRandomCard(number: string, valid: string, cvv: string, limit: number): string {
+    const total = limit - number.length;
+    let digits = ''
+    for (let i = 0; i < total; i++)
+      digits += this.random(0, 9);
 
-  //   number += digits;
+    number += digits;
 
-  //   number = Luhn.generateCheckDigit(number);
-  //   valid = (valid != null && valid.length > 6) ? valid : this.generateValidThru();
-  //   cvv = (cvv != null && cvv.length > 2) ? cvv : this.generateCVV();
+    number = Luhn.generateCheckDigit(number);
+    valid = (valid != null && valid.length > 6) ? valid : this.generateValidThru();
+    cvv = (cvv != null && cvv.length > 2) ? cvv : this.generateCVV();
 
 
-  //   return `${number}|${valid}|${cvv}`;
-  // }
+    return `${number}|${valid}|${cvv}`;
+  }
 
-  // public generateSequencialCard({ number, valid, cvv, range, limit }) {
-  //   const total = limit - number.length;
-  //   const max = limit > 14 ? 12 : 11;
-  //   const min = limit > 14 ? 10 : 9;
-  //   let cards = [];
-  //   if (number.length > min && number.length < max) {
-  //     for (let i = 1; i < range; i++) {
-  //       let digits = this.generateSequence(i, total);
-  //       console.log(digits);
-  //       let aux = Luhn.generateCheckDigit(`${number}${digits}`);
-  //       let v = (valid != null && valid.length > 6) ? valid : this.generateValidThru();
-  //       let c = (cvv != null && cvv.length > 2) ? cvv : this.generateCVV();
+  public generateSequencialCard(number: string, valid: string, cvv: string, range: number, limit: number): Array<string> {
+    const total = limit - number.length;
+    const max = limit > 14 ? 12 : 11;
+    const min = limit > 14 ? 10 : 9;
+    let cards = [];
+    if (number.length > min && number.length < max) {
+      for (let i = 1; i < range; i++) {
+        let digits = this.generateSequence(i, total);
+        console.log(digits);
+        let aux = Luhn.generateCheckDigit(`${number}${digits}`);
+        let v = (valid != null && valid.length > 6) ? valid : this.generateValidThru();
+        let c = (cvv != null && cvv.length > 2) ? cvv : this.generateCVV();
 
-  //       cards.push(`${aux}|${v}|${c}`);
-  //     }
-  //   }
-  //   return cards;
-  // }
+        cards.push(`${aux}|${v}|${c}`);
+      }
+    }
+    return cards;
+  }
 
   public generateSequence(index: number, limit: number) {
     let digits = `${index}`;
